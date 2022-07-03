6c14d564806b5215c39cb29a2eeffa97e6495cd4
Okoye John <okoyejohnkenechukwu@gmail.com>
2022-07-03 14:00:21 -0700

    ,reest

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int  t;
+
+	for (t = 0; t < 10; t++)
+	{
+		printf("%d", t);
+	}
+
+	printf("\n");
+
+	return (0);
+
+
+}
+
+
+
