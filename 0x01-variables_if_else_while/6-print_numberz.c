2b39034feba5b8a56a87c963df24bf8b58cfea46
Okoye John <okoyejohnkenechukwu@gmail.com>
2022-07-03 14:26:16 -0700

    rest2

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	char t;
+
+	for (t = '0'; t <= '9'; t++)
+	{
+		putchar(t);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+
+}
+
