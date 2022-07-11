92f2725b1aa144d44ab7d7e8b7f78dd20f074e13
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-07-11 19:41:26 +0100

    Create 2-mul.c
    checking

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* mul - function to multiply two numbers: a and b
+* @a, b: argments to be multiplied
+* Return: 0 (Success)
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
