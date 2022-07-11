c88c880c65d7defaa771ca34b8c1179618368f67
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-07-11 20:31:06 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,17 @@
+#include "main.h"
+/**
+* print_numbers - function to print numbers 0 to 9
+* Return: void
+*/
 
+void print_numbers(void)
+{
+	int c;
+	
+	for (c >= 0 && c <= 9)
+	{
+		_putchar(c);
+		_putchar('\n');
+	}
+}
+  
