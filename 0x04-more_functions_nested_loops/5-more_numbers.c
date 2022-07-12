834498507299fab0a9ff62a3c84036d055e03169
Okoye John <okoyejohnkenechukwu@gmail.com>
2022-07-11 17:59:48 -0700

    d

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,7 +8,7 @@ void more_numbers(void)
 {
 	int a = 0;
 	int b = 0;
-	
+
 	while (b <= 9)
 	{
 		while (a <= 14)
@@ -17,7 +17,7 @@ void more_numbers(void)
 			{
 				_putchar(a / 10 + '0');
 			}
-			
+
 			_putchar(a % 10 + '0');
 			a++;
 		}
