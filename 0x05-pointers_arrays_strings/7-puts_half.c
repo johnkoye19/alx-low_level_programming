46890c25269670dd3575fea7fb80cda6fb03872d
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-07-14 03:16:48 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,38 @@
-b
+#include "main.h"
+/**
+  * puts_half - function to print a string
+  * @str: variable that stores the location of the string
+  * Return: does not return a value
+  */
+
+void puts_half(char *str)
+{
+	int a = 0;
+	int length_of_the_string;
+	int n;
+
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+	
+	length_of_the_string = a;
+	n = (length_of_the_string - 1) / 2;
+	
+	if (length_of_the_string % 2 == 0)
+		{
+			for (i = a / 2; i <= a - 1; i++)
+			{
+				_putchar(str[i]);
+			}
+		}
+	else
+		{
+			for (i = n + 1; i <= a - 1; i++)
+			{
+				_putchar(str[i]);
+			}
+		}
+
+	_putchar('\n');
+}
