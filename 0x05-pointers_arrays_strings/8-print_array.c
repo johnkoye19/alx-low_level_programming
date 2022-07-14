29f13341fcc2f6ee8c545ffa391724e410e59ed7
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-07-14 04:46:08 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -6,19 +6,19 @@
 
 void print_array(int *a, int n)
 {
-int i;
-
-for (i = 0; i <= n - 1; i++)
-{
-if (i < n - 1)
-{
-printf("%d, ", a[i]);
-}
-else 
-{
-printf("%d\n", a[i]);
-}
-}
+	int i;
+	
+	for (i = 0; i <= n - 1; i++)
+	{
+		if (i < n - 1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d\n", a[i]);
+		}
+	}
 }
 	
 	
