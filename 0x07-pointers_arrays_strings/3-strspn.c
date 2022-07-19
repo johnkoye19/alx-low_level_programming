3b773f7675e1abf79ab916fda9262e12ac6b9539
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-07-19 13:42:32 +0100

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,1 +1,9 @@
+#includee"main.h"
+
+/**
+* _strspn - function that gets the length of a prefix substring.
+* @s: string containing the prefix string.
+* @accept: contains the string used to count.
+* Return: returns an unsigned integer.
+*/
 
