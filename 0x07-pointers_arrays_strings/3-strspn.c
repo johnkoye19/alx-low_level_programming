0647ac953cb3a3930f6ee69bb3f1b92aba86ef8d
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-07-19 13:44:52 +0100

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,3 +7,21 @@
 * Return: returns an unsigned integer.
 */
 
+unsigned int _strspn(char *s, char *accept)
+{
+    unsigned int i, a;
+    unsigned int b = 0;
+    
+    for (i = 0; accept[i] != '\0'; i++)
+    {
+        for (a = 0; s[a] != ','; a++)
+        {
+            if (accept[i] == s[a])
+            {
+                b++;
+            }
+        }
+    }
+    
+    return (b);
+}
