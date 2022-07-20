2ecbfa9d6bb3fa21741295dbd02077ac42f74f2d
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-07-20 16:45:14 +0100

    Create 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,7 @@
+/**
+* _print_rev_recursion - function that prints a string in reverse.
+* @s: the string we are working with. 
+* Return: returns nothing. 
+*/
+
+void _print_rev_recursion(char *s);
