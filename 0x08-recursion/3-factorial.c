9a493fc422ab996122fd5284db09fccf62afbadc
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-07-20 16:55:40 +0100

    Create 3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* factorial - function that returns the factorial of a number.
+* @n: the given number 
+* Return: returns an integer, -1 if n<0.
+*/
+Prototype: int factorial(int n);
+/**
+* If n is lower than 0, 
+* the function should return -1 to indicate an error
+* Factorial of 0 is 1
+*/
