395750f1fea069ed8b9768dc85e99ddfaaebadc9
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-07-20 17:20:59 +0100

    Create 100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/** 
+* is_palindrome - function that returns 1 if a string is a palindrome and 0 if not.
+* @s: the string we are testing. 
+* Return: it returns an integer.
+*/
+
+int is_palindrome(char *s);
+/*An empty string is a palindrome*/
