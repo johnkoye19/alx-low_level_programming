9a09cf59a86c39597c9da8f4e9b2d5e7b142e308
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-07-20 17:05:47 +0100

    Create 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h" 
+
+/**
+* _pow_recursion - function that returns the value of x raised to the power of y.
+* @x: the base of the power y.
+* @y: the power.
+* Return: returns an integer.
+*/
+
+int _pow_recursion(int x, int y);
+/**
+* If y is lower than 0, 
+* the function should return -1
+*/
