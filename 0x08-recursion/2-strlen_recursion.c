5cc45ffdb7bf0c0aaad3c93e8bf3000be6fc969b
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-07-21 02:36:38 +0100

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,4 +5,9 @@
 * Return: returns an integer value.
 */
 
-int _strlen_recursion(char *s);
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
