8e86c12df4292c8ddec9e667da390b7ca96992ac
Okoye John <okoyejohnkenechukwu@gmail.com>
2022-07-20 19:25:08 -0700

    d

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * _strlen_recursion - function that returns the length of a string.
-* @s: string we are working with. 
+* @s: string we are working with.
 * Return: returns an integer value.
 */
 
