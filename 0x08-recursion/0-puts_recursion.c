1e1f51e728aa0611ce6e42d6a2af1e5ae65bae5d
Okoye John <okoyejohnkenechukwu@gmail.com>
2022-07-20 19:11:49 -0700

    g

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * _puts_recursion -  a function that prints a string.
-* @s: the string we are working with 
+* @s: the string we are working with
 * Return: returns void.
 */
 void _puts_recursion(char *s)
@@ -11,7 +11,7 @@ void _puts_recursion(char *s)
 		_putchar('\n');
 		return;
 	}
-	
+
 	_putchar(*s);
-	_puts_recursion(s++);
+	_puts_recursion(s + 1);
 }
