34dd0833755f95deeec2c8a3c996b6b1f2319580
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-07-21 04:51:06 +0100

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+int increse_divide(int n, int i);
 
 /**
 * is_prime_number - function to returns 1 if the n is a prime num, otherwise return 0.
@@ -6,4 +7,36 @@
 * Return: returns an integer.
 */
 
-Prototype: int is_prime_number(int n);
+int is_prime_number(int n)
+{
+	if (n  == 1)
+	{
+		return (0);
+	}
+	
+	if (n == 2)
+	{
+		return (1);
+	}
+	return (increse_divide(n, 2));
+}
+/**
+* increse_divide - function to increment dividend
+* @n: given number.
+* @i: dividend.
+* Return: return int.
+*/
+int increse_divide(int n, int i)
+{
+	if (n > i)
+	{
+		if (n % i == 0)
+		{
+			return (0);
+		}
+		increse_divide(n, i++);
+	}
+	
+	return (1);
+}
+		
