48c816c462e04f46ddc487437f4f951d0ba68350
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-07-21 01:36:58 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,3 +5,16 @@
 * Return: returns void.
 */
 void _puts_recursion(char *s);
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(*s++);
+	}
+}
