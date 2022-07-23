3196ab712fd610df43a8376045fc55ceaed73529
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-07-23 02:05:07 +0100

    Update _putchar.c

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,16 +1,1 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-/**
-  * main - Entry point
-  *
-  * Return: Always 0 (Success)
-  */
-
-int main(void)
-{
-	printf("_putchar\n");
-
-	return (0);
-}
+int _putchar(char c);
