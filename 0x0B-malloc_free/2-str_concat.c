712dc71a657fd379387e7658e684d177a17c9668
Okoye John <okoyejohnkenechukwu@gmail.com>
2022-07-26 06:45:26 -0700

    n

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,1 +1,12 @@
-Third
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  * str_concat -  function to concatenate two strings.
+  * @s1: the first string.
+  * @s2: the second string.
+  * Return: returns apointer to the concat
+  */
+
+char *str_concat(char *s1, char *s2)
