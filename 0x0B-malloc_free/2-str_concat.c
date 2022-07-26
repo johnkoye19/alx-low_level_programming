dce46f902fc6010d282007f80315a2f0d91c65b8
Okoye John <okoyejohnkenechukwu@gmail.com>
2022-07-26 07:55:08 -0700

    g

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,11 +19,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		s1 = '\0';
+		a = 0;
 	}
 	if (s2 == NULL)
 	{
-		s2 = '\0';
+		b = 0;
 	}
 	ptr =  (char *)malloc(sizeof(char) * (a + b + 1));
 
