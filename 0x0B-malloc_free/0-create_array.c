af91701921b1701734febdfdbf3e600781736ffa
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-07-26 10:55:38 +0100

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,8 +10,14 @@
 
 char *create_array(unsigned int size, char c)
 {
+if (size == 0);
+{
+return (NULL);
+}
 char* ptr = (char*) malloc(sizeof(char) * size);
 for (i = 0; i <= size; i++)
 {
 *(ptr + i) = c;
+}
+return (ptr);
 }
