2d209789b35f3aa49ee11b3912228b7c7ce061cc
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-07-26 10:50:18 +0100

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,1 +1,17 @@
-/first allocation 
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+* create_array - function that creates  a string and initializes it.
+* @size: the number of elements 
+* @c: the specific character 
+*  Return: a pointer to the memory location. 
+*/
+
+char *create_array(unsigned int size, char c)
+{
+char* ptr = (char*) malloc(sizeof(char) * size);
+for (i = 0; i <= size; i++)
+{
+*(ptr + i) = c;
+}
