2d58f95c4f64775cfdcaf08562ae51c5c03ea2e1
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-07-26 10:18:32 +0100

    Update main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,4 +1,5 @@
-
+#ifndef MAIN_H
+#define MAIN_H
 
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
@@ -7,3 +8,5 @@ int **alloc_grid(int width, int height)
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
+
+#endif
