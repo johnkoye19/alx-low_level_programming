d403f819f1dd7a0279d5ffeb8e9788f6e5a5b3d5
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-07-26 14:33:50 +0100

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,7 +17,7 @@ char *_strdup(char *str)
 		return (NULL);
 	}
 
-ptr = (char *) malloc(sizeof(char) * (strlen(str) +  1));
+	ptr = (char *) malloc(sizeof(char) * (strlen(str) +  1));
 
 
 	if (ptr == NULL)
