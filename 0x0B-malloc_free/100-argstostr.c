438163dfac23b4681fe372ff9ec9af9bde90bb1f
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-07-27 22:46:03 +0100

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,1 +1,23 @@
-Sixth 
+#include <stlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+* argstostr - function that concatenates all the arguments of your program.
+* @ac:
+* @av:
+* Return: returns a pointer to a new string.
+*/
+
+char *argstostr(int ac, char **av)
+{
+	char *ptr;
+	int i;
+	int j;
+	
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; j 
+		ptr = (char *) malloc(sizeof(char) * (ac + 1));
+	}
+}
