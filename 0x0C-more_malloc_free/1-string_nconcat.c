57bb91534756b509063e5c893e9882283e15b0a9
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-07-28 23:58:43 +0100

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,1 +1,29 @@
-second
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int a, b, i, q;
+	int j = 0;
+	char *ptr;
+	
+	a = (s1 == NULL) ? 0 : strlen(s1);
+	b = (s2 == NULL) ? 0 : strlen(s2);
+	if (b <= n)
+	{
+		n = b;
+	}
+	q = (a + n + 1);
+	ptr = (char *) malloc(sizeof(char) * q);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < q; i++, j++)
+	{
+		*(ptr + j) = s1[i];
+	}
+	for (i = 0; i < q; i++, j++)
+	{
+		*(ptr + j) = s2[i];
+	}
+	*(ptr + q) = '\0';
+	return (ptr);
+}
