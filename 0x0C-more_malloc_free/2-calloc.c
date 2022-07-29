7924c231d02a69425ab43c84e11eb38029c2cb8a
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-07-29 01:21:56 +0100

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,33 +1,33 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
-* _calloc - function that allocates memory for an array, using malloc.
-* @nmemb: the number of elements.
-* @size: the size of one element.
-* Return: returns a pointer to the array.
-*/
-
+ * _calloc - Allocates memory for an array of a certain number
+ * of elements each of an inputted byte size.
+ * @nmemb: The number of elements.
+ * @size: The byte size of each array element.
+ *
+ * Return: If nmemb = 0, size = 0, or the function fails - NULL.
+ * Otherwise - a pointer to the allocated memory.
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i;
-	int *ptr;
-	
+	void *mem;
+	char *filler;
+	unsigned int index;
+
 	if (nmemb == 0 || size == 0)
-	{
 		return (NULL);
-	}
-	
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
-	{
+
+	mem = malloc(size * nmemb);
+
+	if (mem == NULL)
 		return (NULL);
-	}
-	for (i = 0; i < nmemb; i++)
-	{
-		*(ptr + i) = 0;
-	}
-	return (ptr);
+
+	filler = mem;
+
+	for (index = 0; index < (size * nmemb); index++)
+		filler[index] = '\0';
+
+	return (mem);
 }
