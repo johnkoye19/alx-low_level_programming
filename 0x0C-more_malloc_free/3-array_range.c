8c3ef7ee80d1b6d0d973222c1eaa7b3aba3a093a
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-07-29 01:34:48 +0100

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,1 +1,28 @@
-fourth
+Write a function that creates an array of integers.
+
+int *array_range(int min, int max)
+{
+	if (min > max)
+	{
+		return (NULL);
+	}
+		
+	for (i = min; i <= max; i++)
+	{
+		num++;
+	}
+	ptr = (int *) malloc(sizeof(int) * num);
+	
+	if (ptr == NULL)
+	{
+		return (ptr);
+	}
+	for (i = min; i <= max; i++; j++)
+	{
+		ptr[j] = i;
+	}
+}
+The array created should contain all the values from min (included) to max (included), ordered from min to max
+Return: the pointer to the newly created array
+If min > max, return NULL
+If malloc fails, return NULL
