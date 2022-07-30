0d4cc6e2d86421a1029cef9470decd809fe86356
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-07-30 01:17:05 +0100

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,7 +8,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *p;
-	unsigned i;
+	unsigned int index;
 	
 	if (old_size == new_size)
 	{
@@ -17,6 +17,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		ptr = malloc(new_size);
+		
+		if (ptr == NULL)
+		{
+			return (NULL);
+		}
+		return (ptr);
 	}
 	if (ptr != NULL && new_size == 0)
 	{
@@ -24,15 +30,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 	p = malloc(new_size);
-	for (i = 0; )
+	for (index = 0; index < old_size && index < new_size; index++)
+	{
+		p[index] = ptr[index];
+	}
+	free(ptr);
+	return (p);
 }
-where ptr is a pointer to the memory previously allocated with a call to malloc: malloc(old_size)
-old_size is the size, in bytes, of the allocated space for ptr
-and new_size is the new size, in bytes of the new memory block
-The contents will be copied to the newly allocated space, in the range from the start of ptr up to the minimum of the old and new sizes
-If new_size > old_size, the “added” memory should not be initialized
-If new_size == old_size do not do anything and return ptr
-If ptr is NULL, then the call is equivalent to malloc(new_size), for all values of old_size and new_size
-If new_size is equal to zero, and ptr is not NULL, then the call is equivalent to free(ptr). Return NULL
-Don’t forget to free ptr when it makes sense
-FYI: The standard library provides a different function: realloc. Run man realloc to learn more.
