53b595d11d843210b2a1b648eae1aff4d5e1c796
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-08-01 02:45:56 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,8 +1,59 @@
-Write a program that multiplies two positive numbers.
+#include <string.h>
+#include <stdio.h>
+#include <stdlib>
+#include "main.h"
 
-Usage: mul num1 num2
-num1 and num2 will be passed in base 10
-Print the result, followed by a new line
-If the number of arguments is incorrect, print Error, followed by a new line, and exit with a status of 98
-num1 and num2 should only be composed of digits. If not, print Error, followed by a new line, and exit with a status of 98
-You are allowed to use more than 5 functions in your file
+int main(int argc, int *argv[])
+{
+	If (argc > 3)
+	{
+		_putchar(E);
+		_putchar(r);
+		_putchar(r);
+		_putchar(o);
+		_putchar(r);
+		_putchar('\n');
+		exit(98);
+	}
+
+	for (i = 1; i < 3; i++)
+	{
+		for(j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+			{
+				_putchar(E);
+				_putchar(r);
+				_putchar(r);
+				_putchar(o);
+				_putchar(r);
+				_putchar('\n');
+				exit(98);
+			}
+		}
+	mult(*argv[1], *argv[2]);
+	return (0);
+}
+
+int convert(char s1[], char s2[])  
+{
+	int num = 0;
+	int num2 = 0;
+	int n = strlen(s1);
+	int n2 = strlen(s2); 
+	
+	for (int i = 0; i < n; i++)
+	{
+		num = num * 10 + (s1[i] - 48);
+	}
+	for (int i = 0; i < n; i++)
+	{
+		num2 = num2 * 10 + (s2[i] - 48);
+	}
+	answer = mult(int num, int num2);
+	
+}
+int mult(int a, int b)
+{
+return(a * b);
+}
