f2017995183cd800a61c6e1470b257961ef03911
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-08-01 15:51:51 +0100

    Update 4-sum.h

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,5 +1,15 @@
 #ifndef SUM_H
 #define SUM_H
 
-SUM(x, y) ((x) + (y))
+/**
+ * File: 4-sum.h
+ * SUM is a constant that calculates sum of x and y
+ * Desc: Header file that defines a function-like macro SUM(x, y)
+ * that computes the sum of the numbers x and y.
+ * @x: an integer
+ * @y: an integer
+ */
+
+#define SUM(x, y) ((x) + (y))
+
 #endif
