57171c2e4cf590ef1e72bf6a3bbb37fe60674107
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-08-01 03:14:32 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,7 +5,9 @@
 
 int main(int argc, int *argv[])
 {
-	If (argc > 3)
+	int i, j;
+	
+	if (argc > 3)
 	{
 		_putchar(E);
 		_putchar(r);
@@ -31,16 +33,19 @@ int main(int argc, int *argv[])
 				exit(98);
 			}
 		}
-	mult(*argv[1], *argv[2]);
+	convert(*argv[1], *argv[2]);
 	return (0);
 }
 
 int convert(char s1[], char s2[])  
 {
 	int num = 0;
+	int answer;
 	int num2 = 0;
 	int n = strlen(s1);
-	int n2 = strlen(s2); 
+	int n2 = strlen(s2);
+	int i, rem, len = 0, n;
+	char str[];
 	
 	for (int i = 0; i < n; i++)
 	{
@@ -52,8 +57,28 @@ int convert(char s1[], char s2[])
 	}
 	answer = mult(int num, int num2);
 	
+	n = answer;
+	while (n != 0)
+	{
+        	len++;
+        	n /= 10;
+    	}
+    	for (i = 0; i < len; i++)
+    	{
+		rem = num % 10;
+		num = num / 10;
+		str[len - (i + 1)] = rem + '0';
+    	}
+	str[len] = '\0';
+	
+	for (m = 0; m < len; m++)
+	{
+		_putchar(s[m]);
+	}
+	_putchar('\n');
+	return(0);
 }
 int mult(int a, int b)
 {
-return(a * b);
+	return(a * b);
 }
