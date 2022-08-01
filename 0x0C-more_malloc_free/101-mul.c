b95aaca35d08b57ba29fee9495b79e639243468e
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-08-01 03:44:06 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,88 +1,99 @@
-#include <string.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include <stdio.h>
+#include "holberton.h"
 
-int convert(char s1[], char s2[]);
-int mult(int a, int b);
+/**
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_digit(char *s)
+{
+	int i = 0;
 
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
-	int i, j;
-	
-	if (argc > 3)
-	{
-		_putchar('E');
-		_putchar('r');
-		_putchar('r');
-		_putchar('o');
-		_putchar('r');
-		_putchar('\n');
-		exit(98);
-	}
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	for (i = 1; i < 3; i++)
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		for(j = 0; argv[i][j] != '\0'; j++)
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
-			{
-				_putchar('E');
-				_putchar('r');
-				_putchar('r');
-				_putchar('o');
-				_putchar('r');
-				_putchar('\n');
-				exit(98);
-			}
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-	convert(*argv[1], *argv[2]);
-	return (0);
-}
-
-int convert(char *s1[], char *s2[])  
-{
-	int num = 0;
-	int answer;
-	int num2 = 0;
-	int n = strlen(s1);
-	int n2 = strlen(s2);
-	int i, rem, len = 0, m;
-	char *str[];
-	
-	for (i = 0; i < n; i++)
+	for (i = 0; i < len - 1; i++)
 	{
-		num = num * 10 + (s1[i] - 48);
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
 	}
-	for (i = 0; i < n2; i++)
-	{
-		num2 = num2 * 10 + (s2[i] - 48);
-	}
-	answer = mult(num, num2);
-	
-	n = answer;
-	while (n != 0)
-	{
-        	len++;
-        	n /= 10;
-    	}
-    	for (i = 0; i < len; i++)
-    	{
-		rem = num % 10;
-		num = num / 10;
-		str[len - (i + 1)] = rem + '0';
-    	}
-	str[len] = '\0';
-	
-	for (m = 0; m < len; m++)
-	{
-		_putchar(str[m]);
-	}
+	if (!a)
+		_putchar('0');
 	_putchar('\n');
-	return(0);
-}
-int mult(int a, int b)
-{
-	return(a * b);
+	free(result);
+	return (0);
 }
