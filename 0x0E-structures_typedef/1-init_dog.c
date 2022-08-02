72ba3c4c1d03ccf9e246877c419635ad145b140a
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-08-02 05:31:26 +0100

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,10 +3,10 @@
 #include "dog.h"
 /**
 * init_dog - function that initializes the var of struct dog.
-* dog *d: the pointer to the location of the initialization.
-* name: the name in struct dog
-* age: the age in struct dog
-* owner: the owner in struct dog
+* @d: the pointer to the location of the initialization.
+* @name: the name in struct dog
+* @age: the age in struct dog
+* @owner: the owner in struct dog
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
