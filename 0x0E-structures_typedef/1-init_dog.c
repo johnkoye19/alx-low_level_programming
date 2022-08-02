d1b0e782b4efa8695cdc21bf9b243b6f2496bece
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-08-02 05:17:21 +0100

    Create 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* init_dog - function that initializes the var of struct dog.
+* dog *d: the pointer to the location of the initialization.
+* name: the name in struct dog
+* age: the age in struct dog
+* owner: the owner in struct dog
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = &dog_t;
+	dog_t.name = name;
+	dog_t.age = age;
+	dog_t.owner = owner;
+}
