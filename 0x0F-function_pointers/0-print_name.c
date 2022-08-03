6ab4d994bfaaacb138be239a7cdc2e03f8e46634
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-08-03 22:25:49 +0100

    Create 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+* print_name - function to print a name.
+* @name: the name to be printed.
+* @f: a function that accesses name
+* Return: returns nothing
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
