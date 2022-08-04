130216532edd0e6fc062217f2e8d830c2800c65d
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-08-04 15:41:09 +0100

    Create 3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+/**
+* main - function that does basic arithemetics.
+* @argc: int value of the screen parameters.
+* @argv: pointer to string of screen parameters
+* Return: returns an integer
+*/
+
+int main(argc, *argv)
+{
+if (argc < 3)
+{
+error();
+exit(98);
+}
+}
