0fe6a2e788dfddcdb1a405378e5ff4f502dd15d7
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-08-04 11:40:08 +0100

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,2 +1,18 @@
 #include "3-calc.h"
-int (*get_op_func(char *s))(int, int);
+/**
+* get_op_func - gets the op function
+* @s: the operator character
+* Return: returns an integer.
+*/
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	    };
+	int i;
+}
