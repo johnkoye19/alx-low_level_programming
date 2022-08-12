96cff006a12ae0e316580d36f2ce6c5a7cf98443
Okoye John <okoyejohnkenechukwu@gmail.com>
2022-08-11 20:35:41 -0700

    dfe

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,19 +3,18 @@
 #include "lists.h"
 
 /**
-* list_len - Write a function that returns the number of elements in a linked list
+* list_len - Write a function that returns the number of elements in a list
 * @h: the head link
 * Return: returns the number of nodes
 */
 size_t list_len(const list_t *h)
 {
 	size_t nodenum = 0;
-	
+
 	while (h != NULL)
 	{
-		h = h-> next;
+		h = h->next;
 		nodenum++;
 	}
 	return (nodenum);
 }
-  
