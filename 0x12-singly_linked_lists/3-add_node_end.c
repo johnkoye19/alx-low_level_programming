c600727e0be36ad714c8022dc8072162d7f32b0f
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-08-13 04:27:23 +0100

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,37 +1,36 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
 /**
-* add_node_end - function that adds a new node at the end of a list_t list
-* @head: the first pointer
-* @str: the string used
-* Return: returns a pointer
-*/
-
+ * add_node_end - adds a new node at the end of a linked list
+ * @head: double pointer to the `list_t` list
+ * @str: string to put in the new node
+ * Return: address of the new element, or NULL if it fails
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *move, *endnode;
-	
-	move = (*head);
-	endnode = malloc(sizeof(list_t));
-	
-	if (endnode == NULL)
+	list_t *new;
+	list_t *temp = *head;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
 		return (NULL);
-	endnode->str = strdup(str);
-	endnode->len = strlen(str);
-	endnode->next = NULL;
-	if (move == NULL)
-		return(endnode);
-	while (move->next)
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = NULL;
+
+	if (*head == NULL)
 	{
-		*head = endnode;
-		move = move->next;
+		*head = new;
+		return (new);
 	}
-	move->next = endnode;
-	
-	
-	return(endnode);
-	
-	
+
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new;
+
+	return (new);
 }
