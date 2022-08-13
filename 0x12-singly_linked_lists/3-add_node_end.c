5287977d58dbbb666ece41072b5882604ded4d77
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-08-13 03:47:49 +0100

    Create 3-add_node_end.c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+//Write a function that adds a new node at the end of a list_t list.
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *move, *endnode;
+	
+	move = (*head);
+	endnode = malloc(sizeof(list_t));
+	
+	if (move == NULL || endnode == NULL)
+		return (NULL);
+	while (move->next)
+	{
+		move = move->next;
+	}
+	move->next = endnode;
+	endnode->str = strdup(str);
+	endnode->len = strlen(str);
+	endnode->next = NULL;
+	
+	return(endnode);
+	
+	
+}
