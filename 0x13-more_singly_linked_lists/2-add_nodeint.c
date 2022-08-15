f22e6b3f3d5e3ef2032c2d2de78bfe0f4e8b02a6
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-08-15 13:46:49 +0100

    Create 2-add_nodeint.c

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,4 @@
+Write a function that adds a new node at the beginning of a listint_t list.
+
+Prototype: listint_t *add_nodeint(listint_t **head, const int n);
+Return: the address of the new element, or NULL if it failed
