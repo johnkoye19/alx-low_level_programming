379a23bdca81cae495d5f23c1ccc47f2258be268
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-08-15 12:35:51 +0100

    working on mobile 2

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,15 +1,22 @@
-Write a function that prints all the elements of a listint_t list.
-
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+#include <string.h>
+/**
+* print_listint - function to prints all the elements of a list
+* @h: the location of the first node
+* Return: returns a size_t
+*/
 size_t print_listint(const listint_t *h)
 {
-listint_t *move;
-move = h;
-size_t a = 0;
-while (move != NULL)
-{
-printf("%d", move->n);
-move = move->next;
-a++;
-}
-return (a);
+	listint_t *move;
+	move = h;
+	size_t a = 0;
+	while (move != NULL)
+	{
+		printf("%d", move->n);
+		move = move->next;
+		a++;
+	}
+	return (a);
 }
