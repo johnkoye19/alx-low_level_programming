121992e980c617fb15fdecee0d7818573078513b
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-08-15 10:23:35 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,16 @@
 Write a function that prints all the elements of a listint_t list.
 
-Prototype: size_t print_listint(const listint_t *h);
+size_t print_listint(const listint_t *h)
+{
+listint_t *move;
+move = h;
+while (move != NULL)
+}
 Return: the number of nodes
 Format: see example
 You are allowed to use printf
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
