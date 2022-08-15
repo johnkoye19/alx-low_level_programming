06bf23a2b3dca2f6fe48870940b36352e2a47c82
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-08-15 12:38:58 +0100

    Create 1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+#include <string.h>
+Write a function that returns the number of elements in a linked listint_t list.
+
+Prototype: size_t listint_len(const listint_t *h);
