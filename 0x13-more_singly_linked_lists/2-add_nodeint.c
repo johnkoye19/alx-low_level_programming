ce65e3f9619dceb010313ed941e4d93a93d877e1
Okoye John <okoyejohnkenechukwu@gmail.com>
2022-08-16 06:36:47 -0700

    job

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,7 +4,7 @@
 #include <string.h>
 /**
 * add_nodeint - adds a new node at the start of a list
-* @h: the pointer to the pointer to the head entered. 
+* @head: the pointer to the pointer to the head entered
 * @n: the new integer
 * Return: returns new mode address
 */
@@ -12,13 +12,13 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
-	
+
 	new = malloc(sizeof(listint_t *));
 	if (new == NULL)
 	{
 		return (NULL);
 	}
-	new-> next = (*head);
+	new->next = (*head);
 	new->n = n;
 	(*head) = new;
 	return (new);
