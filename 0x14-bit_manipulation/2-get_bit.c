66241939fa5fc79d8b8c3af5a97c6bfdfef91f22
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-08-18 20:35:59 +0100

    Create 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,5 @@
+Write a function that returns the value of a bit at a given index.
+
+Prototype: int get_bit(unsigned long int n, unsigned int index);
+where index is the index, starting from 0 of the bit you want to get
+Returns: the value of the bit at index index or -1 if an error occured
