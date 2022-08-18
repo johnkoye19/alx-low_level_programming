0f2e66574d67abdacef62a5035d0eee9cd7c0c80
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-08-18 19:50:06 +0100

    Create 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,8 @@
+void print_binary(unsigned long int n);
+Write a function that prints the binary representation of a number.
+
+Prototype: void print_binary(unsigned long int n);
+Format: see example
+You are not allowed to use arrays
+You are not allowed to use malloc
+You are not allowed to use the % or / operators
