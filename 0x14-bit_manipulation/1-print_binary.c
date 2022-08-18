67ebb335d134d3b8074b29ed9541c5805d3463c5
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-08-18 20:12:32 +0100

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,11 +1,16 @@
-void print_binary(unsigned long int n);
-Write a function that prints the binary representation of a number.
-create a division and modulo function
-for loop fill the remainder in 
-then reverse.
+#include "main.h"
 
-Prototype: void print_binary(unsigned long int n);
-Format: see example
-You are not allowed to use arrays
-You are not allowed to use malloc
-You are not allowed to use the % or / operators
+/**
+*print_binary - prints the binary representation of a number
+*@n: number to be printed
+*
+*Return: void
+*/
+
+void print_binary(unsigned long int n)
+{
+if (n > 1)
+	print_binary(n >> 1);
+
+_putchar((n & 1) + '0');
+}
