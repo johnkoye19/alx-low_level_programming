3e7fa12320472719f6a0503921ff9e99c44b9827
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-08-23 20:02:38 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,20 +1,19 @@
+#include "main.h"
 Create a function that creates a file
 int create_file(const char *filename, char *text_content)
 {
-int fd;
+	int fd, y;
 
-fd = open(filename, O_TRUNC | O_WRONLY, 0600);
-if (fd < 0)
-	return (-1);
-if (text_content == NULL)
-{
-fd 
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (fd < 0)
+		return (-1);
+	if (text_content == NULL)
+	{
+	fd 
+	}
+	y = write(fd, text_content, SSIZE_MAX);
+	if (y < 0)
+		return (-1);
+	close(fd);
+	return (1);
 }
-return (1);
-}
-where filename is the name of the file to create and text_content is a NULL terminated string to write to the file
-Returns: 1 on success, -1 on failure (file can not be created, file can not be written, write “fails”, etc…)
-The created file must have those permissions: rw-------. If the file already exists, do not change the permissions.
-if the file already exists, truncate it
-if filename is NULL return -1
-if text_content is NULL create an empty file
